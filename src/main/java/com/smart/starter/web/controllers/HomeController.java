1a52d082811ed3063a98c980038aae96f78300d3
gajamr <rishi.sam000@gmail.com>
2014-10-19 13:02:06 -0500

    Adding initial code

---
+++ src/main/java/com/smart/starter/web/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package com.smart.starter.web.controllers;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home(Locale locale, Model model) {	
+		return "index";
+	}
+	
+}
