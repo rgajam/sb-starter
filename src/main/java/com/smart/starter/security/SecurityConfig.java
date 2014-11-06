8ea23cde21a3a2280e96c0c086bab9fa9da0fb53
gajamr <rishi.sam000@gmail.com>
2014-11-05 22:45:46 -0600

    Added Default Spring Securiy Configuration

---
+++ src/main/java/com/smart/starter/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.smart.starter.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author Rishi, created on Nov 5, 2014
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	public void configAuthBuilder(AuthenticationManagerBuilder builder) throws Exception{
+		builder.inMemoryAuthentication()
+			.withUser("user")
+			.password("password")
+			.roles("USER");
+	}
+}
---
+++ src/main/java/com/smart/starter/security/SecurityWebAppInitializer.java
@@ -0,0 +1,13 @@
+package com.smart.starter.security;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * @author Rishi, created on Nov 5, 2014
+ */
+@ComponentScan
+public class SecurityWebAppInitializer extends
+		AbstractSecurityWebApplicationInitializer {
+
+}
