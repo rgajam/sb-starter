ac7ae3d81b7b3d44b18441aaeba90ce868da2c60
gajamr <rishi.sam000@gmail.com>
2014-11-23 10:54:53 -0600

    Enable Global Method Security

--- src/main/java/com/smart/starter/security/SecurityConfig.java
+++ src/main/java/com/smart/starter/security/SecurityConfig.java
@@ -5,8 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
@@ -16,6 +16,7 @@ import org.springframework.security.crypto.password.StandardPasswordEncoder;
  */
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled=true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -61,6 +62,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.permitAll()
 //				.successHandler(authSuccess) //for HTTP 200 OK response when dealing with RESTful services
 //				.failureHandler(authFailure) //for HTTP 401 UNAUTHORIZED response when dealing with RESTful services
+			.and()
+			.exceptionHandling().accessDeniedPage("/403")
 			.and()
 			.logout()
 				.permitAll();
