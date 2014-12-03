4ffd68e0dea3c804cbc254a5e5d513b5a700fa31
gajamr <rishi.sam000@gmail.com>
2014-12-03 00:34:11 -0600

    Cleaned up code

--- src/main/java/com/smart/starter/web/controllers/HomeCtrl.java
+++ src/main/java/com/smart/starter/web/controllers/HomeCtrl.java
@@ -2,12 +2,9 @@ package com.smart.starter.web.controllers;
 
 import java.util.Locale;
 
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.smart.starter.data.model.User;
 
 /**
  * Handles requests for the application home page.
@@ -20,8 +17,6 @@ public class HomeCtrl {
 	
 	@RequestMapping("/")
 	public String home(Locale locale, Model model) {
-//		User activeUser = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//		model.addAttribute("user", activeUser);
 		return "index";
 	}
 	
--- src/main/java/com/smart/starter/web/controllers/rest/CityRestCtrl.java
+++ src/main/java/com/smart/starter/web/controllers/rest/CityRestCtrl.java
@@ -5,8 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
